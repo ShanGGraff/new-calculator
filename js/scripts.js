6688f84120c2872d68313859a7d946f09aa988ed
Shane Graff <copellius@gmail.com>
2021-06-14 20:35:06 -0700

    WIP: Add functions for subtract, multiply, divide
    Co-authored-by: Elliot McGonigal <elliot@elliotmcgonigal.com>

--- js/scripts.js
+++ js/scripts.js
@@ -24,9 +24,30 @@ function divide(number1, number2) {
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
     const result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
+    const result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
+    const result = divide(number1, number2);
     $("#output").text(result);
   });
 });
